0884194f25a33db6be5dc17659dd03776c809c52
atiq-aib <atiq@aibuddha.in>
2024-11-22 23:18:59 +0530

    :necktie: Update metadata API

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { uploadTextDataset } from '../components/TextComponent.js';
 import { uploadImageDataset } from '../components/ImageComponent.js';
+import { updateMetadata } from '../components/metadataComponent.js';
 
 const router = express.Router();
 
 router.post('/uploadText', uploadTextDataset);
 router.post('/uploadImage', uploadImageDataset);
+router.post('/update/metadata', updateMetadata);
 
 export default router;
