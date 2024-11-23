effee4db5f803f196893765129873ba34f05fe24
Krunal-Karena <krunalkarena26@gmail.com>
2024-11-24 00:14:19 +0530

    Add FetchComponent for retrieving dataset from Arweave

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,11 @@ import express from 'express';
 import { uploadDataset as textData } from '../components/AuthComponent.js';
 import { uploadDataset as ImageData } from '../components/ImageComponent.js';
 import { uploadXlsxAndJSonDataset } from '../components/JsonAndXlsxComponent.js';
-
+import { fetchDataset } from '../components/FetchComponent.js';
 const router = express.Router();
 
 router.post('/upload', textData);
 router.post('/uploadImage', ImageData);
 router.post('/uploadDataSet', uploadXlsxAndJSonDataset);
-
+router.get('/fetchDataset', fetchDataset);
 export default router;
