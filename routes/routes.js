da4281343660458ed75ad231f636d4dfab237792
atiq-aib <atiq@aibuddha.in>
2024-11-24 20:30:08 +0530

    :pencil: Updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,13 @@
 import express from 'express';
-import { uploadTextDataset } from '../components/TextComponent.js';
-import { uploadImageDataset } from '../components/ImageComponent.js';
-import { updateMetadata } from '../components/metadataComponent.js';
-import uploadZip from '../components/uploadZip.js';
+// import { updateMetadata } from '../controllers/metadataComponent.js';
+import uploadZip from '../controllers/uploadZip.js';
 import upload from '../middleware/multer.js';
 
 const router = express.Router();
 
-router.post('/uploadText', uploadTextDataset);
-router.post('/uploadImage', uploadImageDataset);
-router.post('/upload', upload.single('zipfile'), uploadZip);
-router.post('/update/metadata', updateMetadata);
+// router.post('/uploadText', uploadTextDataset);
+// router.post('/uploadImage', uploadImageDataset);
+router.post('/upload/zip', upload.single('zipfile'), uploadZip);
+// router.post('/update/metadata', updateMetadata);
 
 export default router;
