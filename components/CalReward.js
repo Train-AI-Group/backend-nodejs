b79dc7e09c86edfd4434836f219801ed262a9208
Krunal-Karena <krunalkarena26@gmail.com>
2024-11-24 21:40:29 +0530

    calReward API completly working.

--- components/CalReward.js
+++ components/CalReward.js
@@ -9,26 +9,13 @@ const privateKey = "";
 
 const calReward = async (req, res) => {
    const { fieldsOfStudy, domains, tasksOrMethods, cleanOrUnclean } = req.body;
-   // console.log("req.query : ",req.body);
+   // console.log("req.body : ",req.body);
    
    if (!fieldsOfStudy || !domains || !tasksOrMethods || !cleanOrUnclean) {
       return res.status(400).json({ error: 'Missing required parameters.' });
    }
 
-   const processId = await spawn({
-      // The Arweave TXID of the ao Module (your Lua script)
-      module: "",  // Replace with the TXID of the Lua script you uploaded
-      scheduler: "_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA",  // wallet address of the Scheduler Unit
-      signer: createDataItemSigner(privateKey),
-      tags: [
-         { name: "Authority", value: "fcoN_xJeisVsPXA-trzVAuIiqO3ydLQxM-L4XbrQKzY" }, // Messaging Unit for receiving messages
-         { name: "Category", value: "calReward" },  // Your custom tag for this process, can be adjusted
-      ],
-   });
-
-   console.log("Process ID:", processId);
-   await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for 5 seconds
-
+   const processId = "tKALrCUqCkJvlDLFO5fuifrfmbyNti1rkYxZDr89yXE";
    const messageId = await message({
       process: processId,
       tags: [
@@ -59,6 +46,7 @@ const calReward = async (req, res) => {
    
    const numberData = parseFloat(cleanedData);
 
-   res.json({ totalPoints:numberData });
+   res.json({ totalPoints:numberData }); 
 }
+
 export { calReward };
