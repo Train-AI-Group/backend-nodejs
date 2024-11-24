e6853bf0271f672d2bf581a35157fb4010435ed3
Krunal-Karena <krunalkarena26@gmail.com>
2024-11-24 15:27:06 +0530

    Add CalReward functionality with Lua and JavaScript integration

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,12 @@ import { uploadDataset as textData } from '../components/AuthComponent.js';
 import { uploadDataset as ImageData } from '../components/ImageComponent.js';
 import { uploadXlsxAndJSonDataset } from '../components/JsonAndXlsxComponent.js';
 import { fetchDataset } from '../components/FetchComponent.js';
+import { calReward } from '../components/calReward.js';
 const router = express.Router();
 
 router.post('/upload', textData);
 router.post('/uploadImage', ImageData);
 router.post('/uploadDataSet', uploadXlsxAndJSonDataset);
 router.get('/fetchDataset', fetchDataset);
+router.get('/calReward', calReward);
 export default router;
